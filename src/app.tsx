5a58c719531f2be1ac935d004f3b8a8c212943d3
Ajeem Ahmad <ahmedazim7804@gmail.com>
2024-08-31 09:57:04 +0530

    redirect from auth to shseet

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Sheet from "./features/sheet/sheet";
-import AuthDialog from "./features/auth/auth_screen";
+import AuthScreen from "./features/auth/auth_screen";
 
 function App() {
     return (
         <>
             <Routes>
-                <Route path="/auth" element={<AuthDialog></AuthDialog>} />
+                <Route path="/auth" element={<AuthScreen></AuthScreen>} />
+                <Route path="/" element={<Sheet></Sheet>}></Route>
             </Routes>
             <Sheet></Sheet>
         </>
--- src/features/sheet/sheet.tsx
+++ src/features/sheet/sheet.tsx
@@ -5,13 +5,6 @@ import TopBar from "./top_bar";
 import { useNavigate } from "react-router-dom";
 
 export default function Sheet() {
-    // const navigate = useNavigate();
-    // useEffect(() => {
-    //     const token = localStorage.getItem("token");
-    //     if (!token) {
-    //         navigate("/auth");
-    //     }
-    // }, []);
     return (
         <div className="w-full h-full z-0 flex flex-col">
             <TopBar></TopBar>
