01493ab501ee6af3646b480c6970ad5337e617c2
Ajeem Ahmad <ahmedazim7804@gmail.com>
2024-09-02 17:17:40 +0530

    rollback websocket server address

--- src/context/socket_context.tsx
+++ src/context/socket_context.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext } from "react";
 import { io } from "socket.io-client";
 
-const SocketContext = createContext(io("http://13.127.205.104:4000"));
+const SocketContext = createContext(io("https://sih-ws-server.onrender.com"));
 
 export function useSocketContext() {
     return useContext(SocketContext);
